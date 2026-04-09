820fec50a7bdc26223c4f5e90dbf2d7be1e45093
PAG221 <papeabdoulayegaye@gmail.com>
2026-04-09 13:45:02 +0000

    Update db.py

--- db.py
+++ db.py
@@ -3,15 +3,29 @@ import os
 
 def get_db():
     try:
+        host = os.getenv("MYSQLHOST")
+        user = os.getenv("MYSQLUSER")
+        password = os.getenv("MYSQLPASSWORD")
+        database = os.getenv("MYSQLDATABASE")
+        port = os.getenv("MYSQLPORT")
+
+        print("DEBUG DB:")
+        print("HOST:", host)
+        print("USER:", user)
+        print("DB:", database)
+        print("PORT:", port)
+
         conn = mysql.connector.connect(
-            host=os.getenv("MYSQLHOST"),
-            user=os.getenv("MYSQLUSER"),
-            password=os.getenv("MYSQLPASSWORD"),
-            database=os.getenv("MYSQLDATABASE"),
-            port=int(os.getenv("MYSQLPORT", 3306))
+            host=host,
+            user=user,
+            password=password,
+            database=database,
+            port=int(port)
         )
+
+        print("✅ Connexion réussie")
         return conn
 
     except Exception as e:
-        print("❌ Erreur connexion base de données :", e)
+        print("❌ ERREUR DB:", e)
         return None
